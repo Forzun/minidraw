6006b1498193fd8f44ab8ab229c987dd19eedac5
Forzun <vbhavesh219@gmail.com>
2025-06-11 16:25:01 +0530

    jwt-zodSchema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express"; 
 import jwt from "jsonwebtoken";
 import { userMiddleware } from "./middleware/auth";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config"; 
+import { CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
 
 const app = express(); 
 
@@ -9,13 +10,27 @@ app.use(express.json());
 
 app.post("/signup", function(req ,res){ 
 
+    const data = CreateUserSchema.safeParse(req.body); 
+    if(!data.success){ 
+         res.json({ 
+            message:"Incorrect inputs"
+        })
+        return;
+    }
     res.json({ 
-        userId:123
-    })
+        userId:12
+    })  
 })
 
 app.post("/signin", userMiddleware , function(req , res) { 
     
+    const data = SigninSchema.safeParse(req.body); 
+    if(!data.success){ 
+         res.json({ 
+            message:"Incorrect inputs"
+        }) 
+        return;
+    }
     const userId = 1;
     const token = jwt.sign({ 
         userId:userId,
@@ -28,6 +43,13 @@ app.post("/signin", userMiddleware , function(req , res) {
 
 app.post("/room", userMiddleware , function(req , res){ 
     try{
+        const data = CreateRoomSchema.safeParse(req.body); 
+        if(!data.success){ 
+             res.json({ 
+                message:"Incorrect inputs"
+            })
+            return;
+        }
 
         res.json({ 
             roomId:123
