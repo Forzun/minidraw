45831e4d899f6b6c88ee41c424d979417b308304
Forzun <vbhavesh219@gmail.com>
2025-06-11 16:23:20 +0530

    jwt-Schema

---
+++ packages/common/src/type.ts
@@ -0,0 +1,21 @@
+import { z } from "zod"; 
+
+export const CreateUserSchema = z.object({ 
+    username: z.string().min(3).max(30), 
+    password: z.string(), 
+    name:z.string()
+})
+
+export const SigninSchema = z.object({ 
+    username: z.string().min(3).max(30), 
+    password: z.string(), 
+}) 
+
+export const CreateRoomSchema = z.object({ 
+    name: z.string().min(2).max(30)
+})
+
+export type CreateUserSchema = z.infer<typeof CreateUserSchema> 
+export type SigninSchema = z.infer<typeof SigninSchema>
+export type CreateRoomSchema = z.infer<typeof CreateRoomSchema>
+
