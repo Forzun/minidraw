cb7734302446ac3675437dcb575aa94db9007ccb
Forzun <vbhavesh219@gmail.com>
2025-06-12 16:50:53 +0530

    addingPrismaSchema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,23 +3,60 @@ import jwt from "jsonwebtoken";
 import { userMiddleware } from "./middleware/auth";
 import { JWT_SECRET } from "@repo/backend-common/config"; 
 import { CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
+import { prismaClient } from "@repo/db/client";
 
 const app = express(); 
 
 app.use(express.json());
 
-app.post("/signup", function(req ,res){ 
+app.post("/signup", async function(req ,res){ 
+    try{ 
+        const data = CreateUserSchema.safeParse(req.body);
 
-    const data = CreateUserSchema.safeParse(req.body); 
-    if(!data.success){ 
-         res.json({ 
-            message:"Incorrect inputs"
+        if(!data.success){ 
+            res.json({ 
+                message:"Incorrect inputs"
+            })
+            return;
+        }
+
+        const existingUser = await prismaClient.user.findFirst({ 
+            where:{
+                email:req.body.email
+            },
         })
-        return;
+
+        if(existingUser){ 
+            res.json({ 
+                user:existingUser,
+            })
+            return;
+        }
+
+        const user = await prismaClient.user.create({ 
+            data:{ 
+                email:req.body.email, 
+                password:req.body.password,
+                name:req.body.password, 
+                photo: req.body.photo,
+            }
+        })
+
+        if(user){ 
+            res.status(200).json({ 
+                user:user,
+            })
+        }else{ 
+            res.json({ 
+                message:"Something went wrong",
+            })
+        }
+        
+    }catch(error){ 
+        res.json({ 
+            error:error
+        })
     }
-    res.json({ 
-        userId:12
-    })  
 })
 
 app.post("/signin", userMiddleware , function(req , res) { 
