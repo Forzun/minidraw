3bf0d92548c7087d514c07dbdde288393b8373d1
Forzun <vbhavesh219@gmail.com>
2025-06-18 23:17:06 +0530

    backendRoute

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -3,13 +3,12 @@ import { BACKEND_URL } from "../../config";
 import CharRoom from "../../../components/CharRoom";
 
 async function getRoomId(slug: string){ 
-    const response = await axios.get(`http://localhost:3002/room/${slug}`);
+    const response = await axios.get(`${BACKEND_URL}/room/${slug}`);
     return response.data.room.id;
 }
 
 export default async function ChatRoom({params}: {params: {slug: string}}){
     const roomId = await getRoomId(params.slug);
-    console.log(roomId)
 
     return <CharRoom id={roomId}></CharRoom>
 }   
--- apps/web/components/CharRoom.tsx
+++ apps/web/components/CharRoom.tsx
@@ -3,13 +3,11 @@ import { BACKEND_URL } from "../app/config"
 import { ChatRoomClient } from "./ChatRoomClient"
 
 async function getChats(roomId: string){ 
-    const response = await axios.get(`${BACKEND_URL}/chat/9`)
-    // console.log(response.data)
+    const response = await axios.get(`${BACKEND_URL}/chat/${roomId}`)
     return response.data.message
 }
 
 export default async function CharRoom({id}: {id: string}){
-    console.log(id)
     const message = await getChats(id)
 
     return <ChatRoomClient message={message} id={id} ></ChatRoomClient>
